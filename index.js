4ff9d3846f59043cd07a53672fea4c81250a352f
Rahul Giri <raahulgiri815@gmail.com>
2023-01-05 16:29:16 +0545

    Add initial javascript changes

---
+++ index.js
@@ -0,0 +1,33 @@
+//alert("Hello world!");
+//var x=9;
+//console.log(x);
+////x=12;
+//console.log(x);
+//var x=7*7;
+//console.log(x);
+// 
+
+// const x=90;
+// console.log(x);
+// console.log(typeof x);
+// let name="rahul";
+// console.log(typeof name);
+// console.log( name);
+// let bol=true;
+// console.log(bol);
+// console.log(typeof bol);
+// let x=90;
+// let y=10;
+// let z=true;
+// let r=false;
+// let result=x+y+z+r;
+// console.log(result);
+// let name="rahul giri";
+// let y=19;
+// let find =name +y;ß
+// console.log(find);
+const name="rahul";
+const lname="giri";
+console.log("my name is : " +name+ " " +lname+".");
+const mygreeting=(`my name is  ${name} ${lname}`);
+console.log(`Hello Everyone :${mygreeting}`)
